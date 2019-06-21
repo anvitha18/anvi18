49e53518e544e3d81ccf29d31c67b8f293c87575
unknown <sathisusmitha@gmail.com>
2019-06-21 14:27:39 +0530

    first commit

---
+++ src/main/java/com/example/anvi18/Anvi18Application.java
@@ -0,0 +1,13 @@
+package com.example.anvi18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Anvi18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Anvi18Application.class, args);
+	}
+
+}
